738f4e4b7df4e293f4fcfa97781cc6b182a9fe4d
Hwangbambi <26999725@naver.com>
2023-01-11 13:39:25 +0900

    Docs - photopie 프로젝트 생성

---
+++ src/main/java/com/sparta/be_finally/BeFinallyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.be_finally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeFinallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeFinallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/be_finally/BeFinallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.be_finally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeFinallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
