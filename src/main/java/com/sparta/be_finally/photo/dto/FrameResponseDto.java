3c64aa3ee3e388a1d1fda7905087767a77ff0c29
me010suye <108880977+addydfdf@users.noreply.github.com>
2023-01-23 07:44:46 +0900

    Feat - 프레임 코드 추가

--- src/main/java/com/sparta/be_finally/photo/dto/FrameResponseDto.java
+++ src/main/java/com/sparta/be_finally/photo/dto/FrameResponseDto.java
@@ -4,13 +4,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.net.URL;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class FrameResponseDto {
     private int frameNum;
 
+    private String frameUrl;
+
     public FrameResponseDto(int frameNum) {
         this.frameNum = frameNum;
+    }
+
+    public FrameResponseDto (int frameNum, URL frameUrl){
+        this.frameNum = frameNum;
+        this.frameUrl = String.valueOf(frameUrl);
     }
 }
