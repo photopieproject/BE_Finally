239735652d4517ef04e0955b7d6d058c573aec12
Hwangbambi <26999725@naver.com>
2023-01-28 10:12:33 +0900

    Feat #7 - 카카오톡 메세지 공유

--- src/main/java/com/sparta/be_finally/user/dto/KakaoFriendResponseDto.java
+++ src/main/java/com/sparta/be_finally/user/dto/KakaoFriendResponseDto.java
@@ -13,4 +13,11 @@ public class KakaoFriendResponseDto {
     private String profile_nickname; // 프로필 닉네임
     private String profile_thumbnail_image; // 프로필 썸네일(Thumbnail) 이미지, HTTPS만 지원
 
+    public void setKakaoFriend(Long id, String uuid, Boolean favorite, String profile_nickname, String profile_thumbnail_image) {
+        this.id = id;
+        this.uuid = uuid;
+        this.favorite = favorite;
+        this.profile_nickname = profile_nickname;
+        this.profile_thumbnail_image = profile_thumbnail_image;
+    }
 }
