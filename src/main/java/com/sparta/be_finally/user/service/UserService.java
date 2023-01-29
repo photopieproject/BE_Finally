22bf862549dc8ba630182f926b4ff1b6dab5320a
me010suye <108880977+addydfdf@users.noreply.github.com>
2023-01-30 07:14:59 +0900

    Feat- # 27 아이디 찾기 추가

--- src/main/java/com/sparta/be_finally/user/service/UserService.java
+++ src/main/java/com/sparta/be_finally/user/service/UserService.java
@@ -1,10 +1,17 @@
 package com.sparta.be_finally.user.service;
 
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.google.common.collect.Lists;
+import com.sparta.be_finally.config.dto.PrivateResponseBody;
 import com.sparta.be_finally.config.errorcode.StatusCode;
 import com.sparta.be_finally.config.errorcode.UserStatusCode;
 import com.sparta.be_finally.config.exception.RestApiException;
 import com.sparta.be_finally.config.jwt.JwtUtil;
 import com.sparta.be_finally.config.model.AES256;
+import com.sparta.be_finally.config.util.SecurityUtil;
+import com.sparta.be_finally.photo.entity.Photo;
+import com.sparta.be_finally.room.entity.Room;
+import com.sparta.be_finally.room.repository.RoomRepository;
 import com.sparta.be_finally.user.dto.LoginRequestDto;
 import com.sparta.be_finally.user.dto.LoginResponseDto;
 import com.sparta.be_finally.user.dto.SignupRequestDto;
@@ -15,7 +22,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDateTime;
+import java.util.*;
+
+
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +37,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final AES256 aes256;
     String newPhoneNumber = null;
+
 
     // 회원가입
     public void signUp(SignupRequestDto requestDto) {
@@ -75,5 +88,59 @@ public class UserService {
         return new LoginResponseDto.commonLogin(user);
     }
 
+    public PrivateResponseBody findUserNum(String phoneNumber) {
 
+        String storeId = "";
+
+        // 핸드폰번호 암호화
+        try {
+            newPhoneNumber = aes256.encrypt(phoneNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<User> userList = userRepository.findAll();
+
+
+        for (User u : userList) {
+            if (u.getPhoneNumber().equals(newPhoneNumber)) {
+                storeId = u.getUserId();
+                return new PrivateResponseBody(UserStatusCode.AGREE_USER_TYPED, storeId);
+            }
+        }
+            return new PrivateResponseBody(UserStatusCode.FAILE_USERID);
+        }
 }
+
+
+
+
+//        for (User u : userList) {
+//            if (u.getPhoneNumber().equals(newPhoneNumber)) {
+//                storeId = u.getUserId();
+//            }
+//        } return new PrivateResponseBody(UserStatusCode.INVALID_TOKEN, storeId);
+//    }
+
+
+
+//            if (!u.getPhoneNumber().equals(newPhoneNumber)) {
+//                return new PrivateResponseBody(UserStatusCode.FAILE_USERID,userLists);
+//            }
+//            return new PrivateResponseBody(UserStatusCode.AGREE_USER_TYPED, userLists);
+//
+
+
+//    List<User> userList = userRepository.findAll(); // 리스트를 받아옴
+//
+//        for (User u : userList) {
+//                if (u.getPhoneNumber().equals(newPhoneNumber)) {
+//                storeId = u.getUserId();
+//                }
+//                }
+//                return new PrivateResponseBody(UserStatusCode.AGREE_USER_TYPED, storeId);
+//                }
+//                }
+//
+
+
